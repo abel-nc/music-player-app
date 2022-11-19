927942ead9723fbbae165e28059d96c685cea241
Abel Navarro <112418726+abel-nc@users.noreply.github.com>
2022-11-19 15:10:43 -0600

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { MdBuild } from 'react-icons/md'
+
+const Footer = () => {
+    return <h2 className='d-flex align-items-center foot color-primary justify-content-center text-center'>
+        <MdBuild className='me-3' /> Built by Abel Navarro
+    </h2>
+
+}
+
+export default Footer
