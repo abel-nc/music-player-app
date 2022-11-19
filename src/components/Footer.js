21278035c881ffce38d0d36aeb98cfa36205eb4a
Abel Navarro <112418726+abel-nc@users.noreply.github.com>
2022-11-19 15:18:55 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { MdBuild } from 'react-icons/md'
 
 const Footer = () => {
-    return <h2 className='d-flex align-items-center foot color-primary justify-content-center text-center'>
+    return <h2 className='d-flex align-items-center foot text-white justify-content-center text-center'>
         <MdBuild className='me-3' /> Built by Abel Navarro
     </h2>
 
